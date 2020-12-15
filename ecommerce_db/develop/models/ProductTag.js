d559aa815637e50ad00515147a885f0b7af2ade0
Israa Beseiso <israabeseiso@gmail.com>
2020-12-14 22:08:54 -0700

    updated files

---
+++ ecommerce_db/develop/models/ProductTag.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+    id: {
+      type:DataTypes.INT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type:DataTypes.INT,
+      refrences: {
+        model: 'product',
+        key:'id'
+      }
+    }
+  },
+  tag_id: {
+type:DataTypes.INT,
+refrences: {
+model: 'tag',
+key:'id'
+    }
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
